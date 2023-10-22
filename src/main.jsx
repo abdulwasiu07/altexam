0f877bb7a80d4889db919cf7141a0741cac01b9a
Ahmad Tijani <ahmadtijani7777@gmail.com>
2023-10-22 11:21:12 -0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import ErrorBoundary from './errorboundary.jsx'
+import ErrorBoundary from './ErrorSoundary.jsx'
 import App from './App.jsx'
 import './index.css'
 
